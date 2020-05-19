ae4b9cb340ffd53684e8b7caeb70635558e032c8
Paul Gardner-Stephen <paul@servalproject.org>
2020-05-19 20:42:49 +0930

    restore use of zlib extraction in monitor_load for windows

--- src/tools/fpgajtag/util.c
+++ src/tools/fpgajtag/util.c
@@ -28,10 +28,9 @@
 #include <fcntl.h>
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
-#ifdef WINDOWS
-#else
-#include <sys/select.h>
 #include <zlib.h>
+#ifndef WINDOWS
+#include <sys/select.h>
 #endif
 #include "util.h"
 #include "elfdef.h"
@@ -529,10 +528,8 @@ uint32_t read_inputfile(const char *filename)
     static uint8_t bitfile_header[] = {
         0, 9, 0xf, 0xf0, 0xf, 0xf0, 0xf, 0xf0, 0xf, 0xf0, 0, 0, 1, 'a'};
     static uint8_t filebuf[BUFFER_MAX_LEN];
-#ifndef WINDOWS
     static uint8_t uncompressbuf[BUFFER_MAX_LEN];
     static uint8_t gzmagic[] = {0x1f, 0x8b};
-#endif
     static uint8_t elfmagic[] = {0x7f, 'E', 'L', 'F'};
     int inputfd = 0;   /* default input for '-' is stdin */
 
@@ -575,7 +572,6 @@ uint32_t read_inputfile(const char *filename)
             exit(-1);
         }
     }
-#ifndef WINDOWS
     if (!memcmp(input_fileptr, gzmagic, sizeof(gzmagic))) {
         printf("fpgajtag: unzip input file, len %d\n", input_filesize);
         z_stream strm;
@@ -596,7 +592,6 @@ uint32_t read_inputfile(const char *filename)
             goto badlen;
         input_fileptr = uncompressbuf;
     }
-#endif
     if (!memcmp(bitfile_header, input_fileptr, sizeof(bitfile_header))) {
         uint8_t *inputtemp = input_fileptr;
         input_fileptr += sizeof(bitfile_header) - 1;
