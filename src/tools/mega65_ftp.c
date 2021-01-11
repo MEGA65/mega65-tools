c1ff78d1db39c992e0e002f36f729a0f195240f2
Paul Gardner-Stephen <paul@servalproject.org>
2021-01-12 04:10:35 +1030

    fix cache handling error

--- src/tools/mega65_ftp.c
+++ src/tools/mega65_ftp.c
@@ -874,8 +874,8 @@ int read_sector(const unsigned int sector_number,unsigned char *buffer,int noCac
       if (i<SECTOR_CACHE_SIZE) {
 	bcopy(buffer,sector_cache[i],512);
 	sector_cache_sectors[i]=sector_number+n;
+	if (sector_cache_count<(i+1)) sector_cache_count=i+1;
       }
-      if (sector_cache_count<(i+1)) sector_cache_count=i+1;
     }
 
     // Make sure to return the actual sector that was asked for
@@ -913,9 +913,8 @@ int write_sector(const unsigned int sector_number,unsigned char *buffer)
     if (i<SECTOR_CACHE_SIZE) {
       bcopy(buffer,sector_cache[i],512);
       sector_cache_sectors[i]=sector_number;
+      if (sector_cache_count<(i+1)) sector_cache_count=i+1;
     }
-    if (sector_cache_count<(i+1)) sector_cache_count=i+1;
-
     
   } while(0);
   if (retVal) printf("FAIL writing sector %d\n",sector_number);
