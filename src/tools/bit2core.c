a2774e07866ebc5443e166c46a2ed85b61968aa4
Gurce Isikyildiz <Gurce.Isikyildiz@resmed.com.au>
2021-01-29 22:39:16 +1100

    #12: removed skipping of first 120-bytes of bitstream header, so as to match behaviour of m65connect. - also removed an unused/old copy of 'megaflash.c'. The true/latest copy resides in the mega65-core repo

--- src/tools/bit2core.c
+++ src/tools/bit2core.c
@@ -212,7 +212,7 @@ void write_core_file(const int bit_size, const char* core_name, const char* core
     header_block[32 + i] = core_version[i];
 
   fwrite(header_block, CORE_HEADER_SIZE - MAGIC_LEN, 1, of);
-  fwrite(&bitstream_data[BITSTREAM_HEADER_SIZE], bit_size - BITSTREAM_HEADER_SIZE, 1, of);
+  fwrite(bitstream_data, bit_size, 1, of);
   fclose(of);
 
   fprintf(stderr, "Core file written: \"%s\"\n", core_filename);
