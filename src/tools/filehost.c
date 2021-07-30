fc2045a50b726e30894ee881395cf5e1f2692425
Gurce Isikyildiz <Gurce.Isikyildiz@resmed.com.au>
2021-07-31 00:34:35 +1000

    added a function to download a file from the filehost

--- src/tools/filehost.c
+++ src/tools/filehost.c
@@ -223,9 +223,140 @@ void read_filehost_struct(void)
   }
 
   print_items();
+
+  close_tcp_port(fd);
+}
+
+int check_file_start(char c)
+{
+  static char *term = "\r\n\r\n";
+  static int termidx = 0;
+
+  if (c == term[termidx])
+  {
+    termidx++;
+    if (term[termidx] == '\0')
+    {
+      termidx = 0;
+      return 1;
+    }
+  }
+  else
+    termidx = 0;
+
+  return 0;
+}
+
+void check_content_length(char c, int* content_length)
+{
+  static char *term = "Content-Length: ";
+  static int termmatch = 0;
+  static int termidx = 0;
+  static char value[128] = "";
+  static int valueidx = 0;
+
+  if (!termmatch)
+  {
+    if (c == term[termidx])
+    {
+      termidx++;
+      if (term[termidx] == '\0')
+      {
+        termmatch = 1;
+        termidx = 0;
+      }
+    }
+    else
+      termidx = 0;
+  }
+  // checking for value now
+  else
+  {
+    if (c == '\r' || c == '\n')
+    {
+      // We reached the end of the value, now parse it
+      value[valueidx] = '\0';
+      *content_length = atoi(value);
+      // reset vars for next time
+      termmatch = 0;
+      termidx = 0;
+      valueidx = 0;
+    }
+    else
+    {
+      value[valueidx] = c;
+      valueidx++;
+    }
+  }
+}
+
+void download_file(char *path, char *fname)
+{
+  char str[4096];
+  PORT_TYPE fd = open_tcp_port("tcp#files.mega65.org:80");
+  sprintf(str, "GET /php/%s HTTP/1.1\r\n", path);
+  do_write(fd, str);
+  do_write(fd, "Host: files.mega65.org\r\n\r\n");
+
+  do_usleep(100000);
+
+  int count = 0, total = 0;
+  int content_length = 0;
+  int file_started = 0;
+  FILE *f = NULL;
+  while(content_length == 0 || total != content_length)
+  {
+    count = do_read(fd, str, 4096);
+    if (count == 0)
+    {
+      do_usleep(10000);
+      continue;
+    }
+
+    // printf("reading %d bytes...\n", count);
+    for (int k = 0; k < count; k++)
+    {
+      char c = str[k];
+      if (!content_length)
+      {
+        check_content_length(c, &content_length);
+        if (!content_length)
+          continue;
+        printf("content_length=%d\n", content_length);
+      }
+
+      if (!file_started)
+      {
+        // wait for header to finish
+        if (check_file_start(c))
+        {
+          file_started = 1;
+          f = fopen(fname, "wb");
+        }
+      }
+      else
+      {
+        // now start saving out bytes
+        fputc(c, f);
+        total++;
+        if (total == content_length)
+        {
+          printf("Download of file complete\n");
+          break;
+        }
+      }
+    }
+    if (content_length != 0 && total == content_length)
+    {
+      break;
+    }
+  }
+
+  close_tcp_port(fd);
 }
 
 void main(void)
 {
-  read_filehost_struct();
+  // read_filehost_struct();
+  download_file("../files/c/camelot-1536dots_a5uS3h.prg", "camelot.prg");
 }
