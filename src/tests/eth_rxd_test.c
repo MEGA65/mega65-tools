94f1241fc9ba076c343fe043490785d96242dde8
Paul Gardner-Stephen <paul@servalproject.org>
2022-07-05 07:47:53 +0930

    decode eth frames from raw oversampled data

--- src/tests/eth_rxd_test.c
+++ src/tests/eth_rxd_test.c
@@ -94,18 +94,11 @@ unsigned short mdio_read_register(unsigned char addr, unsigned char reg)
   return result;
 }
 
-#define TEST(reg, v, bit, desc)                                                                                             \
-  {                                                                                                                         \
-    if (v & (1 << bit)) {                                                                                                   \
-      snprintf(msg, 160, "   MDIO $%x.%x : %s", reg, bit, desc);                                                            \
-      println_text80(8, msg);                                                                                               \
-    }                                                                                                                       \
-  }
-
 unsigned char free_buffers = 0;
 unsigned char last_free_buffers = 0;
 
-unsigned char frame_buffer[1024];
+unsigned char frame_buffer[2048];
+unsigned char frame_bytes[2048/4];
 char msg[160 + 1];
 
 unsigned short i;
@@ -220,57 +213,6 @@ int to_decimal(unsigned char* src, int bytes)
   return out;
 }
 
-unsigned char text_row = 0;
-
-#ifdef NATIVE_TEST
-void clear_text80(void)
-{
-}
-
-void println_text80(unsigned char colour, char* msg)
-{
-  fprintf(stdout, "%s\n", msg);
-}
-
-unsigned char safe_char(unsigned char in)
-{
-  if (in >= ' ' && in < 0x7d)
-    return in;
-  return 0x2e;
-}
-
-#else
-void clear_text80(void)
-{
-  lfill(0xc000, 4000, 0x20);
-  lfill(0xff80000L, 4000, 1);
-  text_row = 0;
-}
-
-void println_text80(unsigned char colour, char* msg)
-{
-  // XXX debug test
-  return;
-  
-  if (text_row == 49) {
-    lcopy(0xc000 + 3*80, 0xc000 + 2*80, 4000 - 3*80);
-    lcopy(0xff80000 + 3*80, 0xff80000 + 2*80, 4000 - 3*80);
-    lfill(0xc000 + 4000 - 80, 0x20, 80);
-    lfill(0xff80000 + 4000 - 80, 0x01, 80);
-  }
-  print_text80(0, text_row, colour, msg);
-  if (text_row < 49)
-    text_row++;
-}
-
-unsigned char safe_char(unsigned char in)
-{
-  if (!in)
-    return 0x2e;
-
-  return in;
-}
-
 unsigned short frame_counter=0;
 void send_frame(void)
 {
@@ -285,54 +227,25 @@ void send_frame(void)
   // TX
   POKE(0xd6e4,0x01);
 
-  snprintf(msg, 160, "TX test frame $%04x", frame_counter);
-  println_text80(7, msg);
-
-  
   frame_counter++;
 }
   
-
-#endif
-
 unsigned short mdio0 = 0, mdio1 = 0, last_mdio0 = 0, last_mdio1 = 0, frame_count = 0;
 unsigned char phy, last_frame_num = 0, show_hex = 1, last_d6ef = 0;
 
-#ifdef NATIVE_TEST
-int main(int argc, char** argv)
-{
-  if (argc != 2) {
-    fprintf(stderr, "usage: wirekrill <file.pcap>\n");
-    exit(-1);
-  }
-
-  char errbuf[8192];
-  p = pcap_open_offline(argv[1], errbuf);
-  if (!p) {
-    fprintf(stderr, "ERROR: Failed to open PCAP capture file '%s'\nPCAP said: %s\n", argv[1], errbuf);
-    exit(-1);
-  }
-
-#else
 void main(void)
 {
-#endif
   unsigned short len;
   // since the start of the header varies depending on whether the IPv4 options field is present
   unsigned short start_of_protocol_header;
   // looping
   int i;
-
+  
   POKE(0x0286, 0x01);
 
   m65_io_enable();
 
   h640_text_mode();
-
-  clear_text80();
-
-  println_text80(1, "WireKrill 0.0.1 Network Analyser.");
-  println_text80(1, "(C) Paul Gardner-Stephen, 2020-2022.");
 
   // Clear reset on ethernet controller
   POKE(0xD6E0, 0x00);
@@ -359,10 +272,8 @@ void main(void)
     if (mdio_read_register(phy, 1) != 0xffff)
       break;
   if (phy == 0x20) {
-    println_text80(7, "WARNING: Could not find PHY address.");
+    //    println_text80(7, "WARNING: Could not find PHY address.");
   }
-  snprintf(msg, 160, "Network PHY is at MDIO address $%x", phy);
-  println_text80(1, msg);
 
   while (1) {
 
@@ -384,7 +295,6 @@ void main(void)
 	       (PEEK(0xD6EF) >> 0) & 3, (PEEK(0xD6EF) >> 2) & 3, (PEEK(0xD6EF) >> 6) & 3, (PEEK(0xD6EF) >> 4) & 0x3,
 	       (PEEK(0xD6E1)&0x6)>>1,
 	       PEEK(0xD6E3)>>4);
-      if (0) println_text80(1, msg);
       //      while(!PEEK(0xD610)) continue; POKE(0xD610,0);
     }
 
@@ -392,241 +302,82 @@ void main(void)
     if (!(PEEK(0xD6E1) & 0x20)) {
       // POKE(0xD020,PEEK(0xD020)+1);
     } else
-    {
-      // Ethernet frame received      
-      lcopy(0xFFDE800L, (long)frame_buffer, 0x0800);
-
-      lfill((long)msg, 0, 160);
-      len = frame_buffer[0] + ((frame_buffer[1] & 0xf) << 8);
-      snprintf(msg, 160, "#$%02x : Ethernet Frame #%d", PEEK(0xD7FA), ++frame_count);
-      println_text80(1, msg);
-
-      snprintf(msg, 160, "  %02x:%02x:%02x:%02x:%02x:%02x > %02x:%02x:%02x:%02x:%02x:%02x : len=%d($%x), rxerr=%c",
-          frame_buffer[8], frame_buffer[9], frame_buffer[10], frame_buffer[11], frame_buffer[12], frame_buffer[13],
-          frame_buffer[2], frame_buffer[3], frame_buffer[4], frame_buffer[5], frame_buffer[6], frame_buffer[7], len, len,
-          (frame_buffer[1] & 0x80) ? 'Y' : 'N');
-      println_text80(13, msg);
-      if (frame_buffer[16] == 0x45) {
-        // IPv4
-        start_of_protocol_header = 36;
-        snprintf(msg, 160, "  IPv4: %d.%d.%d.%d -> %d.%d.%d.%d", frame_buffer[2 + 14 + 12 + 0], frame_buffer[28 + 1],
-            frame_buffer[28 + 2], frame_buffer[28 + 3], frame_buffer[32 + 0], frame_buffer[32 + 1], frame_buffer[32 + 2],
-            frame_buffer[32 + 3]);
-        println_text80(13, msg);
-        if (frame_buffer[16 + 9] == 0x01) {
-          // ICMP
-          if (frame_buffer[16 + 20] == 0x08) {
-            // PING
-            snprintf(msg, 160, "  ICMP: Echo request, id=%d, seq=%d",
-                frame_buffer[16 + 20 + 5] + (frame_buffer[16 + 20 + 4] << 8),
-                frame_buffer[16 + 20 + 7] + (frame_buffer[16 + 20 + 6] << 8));
-            println_text80(13, msg);
-
-	    lcopy(frame_buffer,0xc000,80);
-	    lfill(0xff80000,1,80);
-	    
-	    for(i=0;i<80;i++) {
-	      char v=((frame_buffer[60+i/4]>>((3^(i&3))<<1))&0x02)?0xa3:0x20;
-	      if (PEEK(0xc000+3*80+i)!=v) POKE(0xc000+7*80+i,PEEK(0xc000+7*80+i)+1);
-	      POKE(0xc000+3*80+i,v);
-	      v=((frame_buffer[60+i/4]>>((3^(i&3))<<1))&0x01)?0xa3:0x20;
-	      if (PEEK(0xc000+4*80+i)!=v) POKE(0xc000+8*80+i,PEEK(0xc000+8*80+i)+1);
-	      POKE(0xc000+4*80+i,v);
-	      POKE(0xc000+5*80+i,'0'+i/4);
-	      POKE(0xc000+1*80+60+i/4,'0'+i/4);
-	      
-	    }
-	    
-            //      show_hex=0;
-          }
-          else if (frame_buffer[16 + 20] == 0x03) // DESTINATION UNREACHABLE
-          {
-            printf("  ICMP: Destination Unreachable, code=%d, nexthopMTU=%d", frame_buffer[start_of_protocol_header + 1],
-                to_decimal(&frame_buffer[start_of_protocol_header + 6], 2));
-          }
-          else if (frame_buffer[16 + 20] == 0x05) // redirect
-          {
-            printf("  ICMP: Redirect, code=%d, ipaddress=%d.%d.%d.%d,", frame_buffer[start_of_protocol_header + 1],
-                frame_buffer[start_of_protocol_header + 4], frame_buffer[start_of_protocol_header + 5],
-                frame_buffer[start_of_protocol_header + 6], frame_buffer[start_of_protocol_header + 7]);
-          }
-        }
-        else if (frame_buffer[16 + 9] == 0x06) // TCP
-        {
-          printf("  TCP Protocol:\n");
-          printf("    source port is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 0], 2));
-          printf("    destination port is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 2], 2));
-          printf("    sequence number is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 4], 4));
-          printf("    ackknowledement number is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 8], 4));
-          printf("    data offset is %d\n", frame_buffer[start_of_protocol_header + 12] >> 4);
-          printf("    flags: ");
-          if (frame_buffer[start_of_protocol_header + 12] & 1) {
-            printf("NS ");
-          }
-          if ((frame_buffer[start_of_protocol_header + 13] >> 7) & 1) {
-            printf("CWR ");
-          }
-          if ((frame_buffer[start_of_protocol_header + 13] >> 6) & 1) {
-            printf("ECE ");
-          }
-          if ((frame_buffer[start_of_protocol_header + 13] >> 5) & 1) {
-            printf("URG ");
-          }
-          if ((frame_buffer[start_of_protocol_header + 13] >> 4) & 1) {
-            printf("ACK ");
-          }
-          if ((frame_buffer[start_of_protocol_header + 13] >> 3) & 1) {
-            printf("PSH ");
-          }
-          if ((frame_buffer[start_of_protocol_header + 13] >> 2) & 1) {
-            printf("RST ");
-          }
-          if ((frame_buffer[start_of_protocol_header + 13] >> 1) & 1) {
-            printf("SYN ");
-          }
-          if ((frame_buffer[start_of_protocol_header + 13] >> 0) & 1) {
-            printf("FIN ");
-          }
-          printf("\n");
-          printf("    window size is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 14], 2));
-          printf("    checksum is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 16], 2));
-          printf("    urgent pointer is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 18], 2));
-
-          // data offset gives us the sie of the tcp header in 32 bit words,
-          // ie to get to the payload we increment by dataoffset * 4 bytes
-          start_of_protocol_header += ((frame_buffer[start_of_protocol_header + 12] >> 4) * 4);
-
-          // HTTP tests
-          // evaulate the payload
-          if (frame_buffer[start_of_protocol_header + 0] == 'P' && frame_buffer[start_of_protocol_header + 1] == 'O'
-              && frame_buffer[start_of_protocol_header + 2] == 'S' && frame_buffer[start_of_protocol_header + 3] == 'T') {
-            printf("HTTP: Post\n");
-            for (i = 5; frame_buffer[start_of_protocol_header + i] != ' '; i++) {
-              putchar(frame_buffer[start_of_protocol_header + i]);
-            }
-            putchar('\n');
-          }
-          else if (frame_buffer[start_of_protocol_header + 0] == 'G' && frame_buffer[start_of_protocol_header + 1] == 'E'
-                   && frame_buffer[start_of_protocol_header + 2] == 'T') {
-            printf("HTTP: Get\n");
-            for (i = 4; frame_buffer[start_of_protocol_header + i] != ' '; i++) {
-              putchar(frame_buffer[start_of_protocol_header + i]);
-            }
-            putchar('\n');
-          }
-        }
-        else if (frame_buffer[16 + 9] == 0x11) // UDP
-        {
-          printf("  UDP Protocol:\n");
-          // udp contains a source port
-          printf("    source port is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 0], 2));
-          // a destination port
-          printf("    destination port is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 2], 2));
-          // length
-          printf("    length is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 4], 2));
-          // checksum
-          printf("    checksum is %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 6], 2));
-        }
-      }
-      else if (frame_buffer[15] == 0x06) // ARP
       {
-        start_of_protocol_header = 16;
-        printf("  ARP protocol:\n");
-        printf("    Hardware type: %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 0], 2));
-        printf("    Protocol type: %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 2], 2));
-        printf("    Hardware address length: %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 4], 1));
-        printf("    Protocol address length: %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 5], 1));
-        printf("    Opcode: %d\n", to_decimal(&frame_buffer[start_of_protocol_header + 6], 2));
-        printf("    Sender Hardware Address: %02x:%02x:%02x:%02x:%02x:%02x\n", frame_buffer[start_of_protocol_header + 8],
-            frame_buffer[start_of_protocol_header + 9], frame_buffer[start_of_protocol_header + 10],
-            frame_buffer[start_of_protocol_header + 11], frame_buffer[start_of_protocol_header + 12],
-            frame_buffer[start_of_protocol_header + 13]);
-        printf("    Sender IP address: %d.%d.%d.%d\n", frame_buffer[start_of_protocol_header + 14],
-            frame_buffer[start_of_protocol_header + 15], frame_buffer[start_of_protocol_header + 16],
-            frame_buffer[start_of_protocol_header + 17]);
-        printf("    Target Hardware Address: %02x:%02x:%02x:%02x:%02x:%02x\n", frame_buffer[start_of_protocol_header + 18],
-            frame_buffer[start_of_protocol_header + 19], frame_buffer[start_of_protocol_header + 20],
-            frame_buffer[start_of_protocol_header + 21], frame_buffer[start_of_protocol_header + 22],
-            frame_buffer[start_of_protocol_header + 23]);
-        printf("    Target IP address: %d.%d.%d.%d\n", frame_buffer[start_of_protocol_header + 24],
-            frame_buffer[start_of_protocol_header + 25], frame_buffer[start_of_protocol_header + 26],
-            frame_buffer[start_of_protocol_header + 27]);
+	char is_broadcast, is_icmp, is_ping;
+	
+	// Ethernet frame received      
+	lcopy(0xFFDE800L, (long)frame_buffer, 0x0800);
+	
+	POKE(0xD020,1);
+	POKE(0xD020,0);
+
+	// 43 = start of eth dest addr
+	#define ETH_DST_OFS 43
+	
+	// Decode frame
+	for(i=0;i<(2048/4);i++) {
+	  unsigned char v=0;
+
+	  v=(frame_buffer[ETH_DST_OFS+i*4+0]&0xc0)>>6;
+	  v|=(frame_buffer[ETH_DST_OFS+i*4+1]&0xc0)>>4; 
+	  v|=(frame_buffer[ETH_DST_OFS+i*4+2]&0xc0)>>2; 
+	  v|=(frame_buffer[ETH_DST_OFS+i*4+3]&0xc0)>>0; 
+
+	  frame_bytes[i]=v;
+	}
+	       	
+	is_broadcast=1;
+	for(i=0;i<6;i++) if (frame_bytes[i]!=0xff) { is_broadcast=0; break; }
+
+	is_icmp=0; is_ping=0;
+        if (frame_bytes[16 + 7] == 0x01) {	  
+          is_icmp=1;
+          if (frame_bytes[16 + 18] == 0x08) {
+	    is_ping=1;
+	  }
+	} 
+	
+	
+#define START_OFS 60
+
+	if (is_broadcast&&is_ping) {
+	  lcopy(frame_buffer,0xc000,80);
+	  lfill(0xff80000,1,80);
+	  lcopy(frame_buffer,0xc800,0x800);
+
+	  // Show individual bits and count differences
+	  for(i=0;i<80;i++) {
+	    char v=((frame_buffer[START_OFS+i/4]>>((3^(i&3))<<1))&0x02)?0xa3:0x20;
+	    if (PEEK(0xc000+3*80+i)!=v) POKE(0xc000+7*80+i,PEEK(0xc000+7*80+i)+1);
+	    POKE(0xc000+3*80+i,v);
+	    v=((frame_buffer[START_OFS+i/4]>>((3^(i&3))<<1))&0x01)?0xa3:0x20;
+	    if (PEEK(0xc000+4*80+i)!=v) POKE(0xc000+8*80+i,PEEK(0xc000+8*80+i)+1);
+	    POKE(0xc000+4*80+i,v);
+	    POKE(0xc000+5*80+i,'0'+i/4);
+	    POKE(0xc000+1*80+START_OFS+i/4,'0'+i/4);
+	  }
+
+	  // Show hex decode
+	  for(i=0;i<100;i++) {
+	    unsigned char v=frame_bytes[i];
+	    unsigned char hex;
+	    
+	    hex=(v&0xf0)>>4; if (hex<10) hex+='0'; else hex-=9;
+	    POKE(0xc000+(10+i/16)*80+((i&0xf)<<2),hex);
+	    hex=(v&0xf); if (hex<10) hex+='0'; else hex-=9;
+	    POKE(0xc000+(10+i/16)*80+1+((i&0xf)<<2),hex);
+	  }
+	  
+	}
+
+	POKE(0xD6E1,3);
+	POKE(0xD6E1,1);
+
+	// Prime for next debug RX frame
+	POKE(0xD6e4,0xd0);
+	POKE(0xD6e4,0xde);
+	
       }
-      if (show_hex) {
-        for (i = 0; i < 256 && i < len; i += 16) {
-          lfill((long)msg, 0, 160);
-          snprintf(msg, 160,
-              "       %04x: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x  "
-              "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-              i, frame_buffer[14 + i], frame_buffer[15 + i], frame_buffer[16 + i], frame_buffer[17 + i],
-              frame_buffer[18 + i], frame_buffer[19 + i], frame_buffer[20 + i], frame_buffer[21 + i], frame_buffer[22 + i],
-              frame_buffer[23 + i], frame_buffer[24 + i], frame_buffer[25 + i], frame_buffer[26 + i], frame_buffer[27 + i],
-              frame_buffer[28 + i], frame_buffer[29 + i],
-
-              safe_char(frame_buffer[14 + i]), safe_char(frame_buffer[15 + i]), safe_char(frame_buffer[16 + i]),
-              safe_char(frame_buffer[17 + i]), safe_char(frame_buffer[18 + i]), safe_char(frame_buffer[19 + i]),
-              safe_char(frame_buffer[20 + i]), safe_char(frame_buffer[21 + i]), safe_char(frame_buffer[22 + i]),
-              safe_char(frame_buffer[23 + i]), safe_char(frame_buffer[24 + i]), safe_char(frame_buffer[25 + i]),
-              safe_char(frame_buffer[26 + i]), safe_char(frame_buffer[27 + i]), safe_char(frame_buffer[28 + i]),
-              safe_char(frame_buffer[29 + i]));
-          println_text80(14, msg);
-        }
-      }
-
-      // Mark frame as received, thus freeing the buffer up
-      POKE(0xD6E1, 0x01);
-      POKE(0xD6E1, 0x03);
-      POKE(0xD6E1, 0x01);
-
-    }
-
-    // Periodically ask for update to MDIO register read
-    if (PEEK(0xD7FA) != last_frame_num) {
-      last_frame_num = PEEK(0xD7FA);
-      POKE(0xD6E6L, 1);
-    }
-
-    // Report MDIO changes
-    mdio1 = PEEK(0xD6E7L);
-    mdio1 |= PEEK(0xD6E8L) << 8;
-    // Work around bug where MDIO register 1 some times reads shifted by one bit
-    if ((mdio1 & 0xf000) == 0xf000)
-      mdio1 = last_mdio1;
-    if ((mdio1 != last_mdio1)) {
-
-      //      TEST(1, mdio1, 15, "T4 capable");
-      //      TEST(1, mdio1, 14, "100TX FD capable");
-      //      TEST(1, mdio1, 13, "100TX HD capable");
-      //      TEST(1, mdio1, 12, "10T FD capable");
-      //      TEST(1, mdio1, 11, "10T HD capable");
-      //      TEST(1, mdio1, 6, "Preamble suppression");
-      if ((mdio1 ^ last_mdio1) & (1 << 5)) {
-        if (mdio1 & (1 << 5))
-          println_text80(5, "PHY: Auto-negotiation completee");
-        else
-          println_text80(7, "PHY: Auto-negotiation in progress");
-      }
-      if ((mdio1 ^ last_mdio1) & (1 << 2)) {
-        if (mdio1 & (1 << 2))
-          println_text80(5, "PHY: Link is up");
-        else
-          println_text80(2, "PHY: Link DOWN");
-      }
-
-      //      TEST(1, mdio1, 5, "Auto-neg complete");
-      //      TEST(1, mdio1, 4, "Remote fault");
-      //      TEST(1, mdio1, 3, "Can auto-neg");
-
-      //      TEST(1, mdio1, 2, "Link is up");
-      //      TEST(1, mdio1, 1, "Jabber detected");
-      //      TEST(1, mdio1, 0, "Supports ex-cap regs");
-
-      last_mdio0 = mdio0;
-      last_mdio1 = mdio1;
-    }
   }
-
-#ifdef NATIVE_TEST
-  return 0;
-#endif
+ 
 }
