af69807b4eb9265f8f9fe744ca65090f9e3d2072
Oliver Graf <ograf@devilsclaw.de>
2022-07-25 16:58:46 +0200

    fix: dput needs to check if file exists before doing anything else #121

--- src/tools/mega65_ftp.c
+++ src/tools/mega65_ftp.c
@@ -2823,7 +2823,8 @@ void list_all_roms(void)
 
 void wrap_upload(char* fname)
 {
-  char* d81name = create_d81_for_prg(fname);
+  char* d81name;
+  if (!(d81name = create_d81_for_prg(fname))) return;
   strcpy(fname, d81name);
 
   if (fname) {
