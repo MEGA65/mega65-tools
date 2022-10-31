cacdc18f427ce121042a3c2e8ab9da46c0c0f3b2
Oliver Graf <ograf@devilsclaw.de>
2022-10-31 13:38:43 +0100

    use jenkins env for version generation

--- src/gitversion.sh
+++ src/gitversion.sh
@@ -2,19 +2,22 @@
 
 # status of branch in short format
 # work around travis and jenkins doing things differently
-if [[ -n $TRAVIS_BRANCH ]]; then
-    branch=$TRAVIS_BRANCH
-elif [[ -n $BRANCH_NAME ]]; then
-    branch=$BRANCH_NAME
+if [[ -n $JENKINS_SERVER_COOKIE ]]; then
+    branch2=${BRANCH_NAME:0:6}
+    version=${GIT_COMMIT:0:7}
 else
-    branch=$(git rev-parse --abbrev-ref HEAD)
+    if [[ -n $TRAVIS_BRANCH ]]; then
+        branch=$TRAVIS_BRANCH
+    else
+        branch=$(git rev-parse --abbrev-ref HEAD)
+    fi
+    # we only take the first 6 chars
+    branch2=${branch:0:6}
+    # get commit (7 chars plus optional ~ for dirty)
+    version=$(git describe --always --abbrev=7 --dirty=~)
 fi
-# we only take the first 6 chars
-branch2=${branch:0:6}
-# get commit (7 chars plus optional ~ for dirty)
-version=$(git describe --always --abbrev=7 --dirty=~)
 # get date plus hour
 datetime=$(date +%Y%m%d.%H)
 
 stringout="${datetime}-${branch2}-${version}"
-echo "Generated Version string: $stringout"
+echo $stringout
