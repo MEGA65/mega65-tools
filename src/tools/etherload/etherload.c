d367f71fd6f5b5bc73c986be0355c3fd5dd21253
Paul Gardner-Stephen <paul@servalproject.org>
2022-11-03 21:25:38 +1030

    implement sending ACK packets back

--- src/tools/etherload/etherload.c
+++ src/tools/etherload/etherload.c
@@ -60,13 +60,12 @@ char all_done_routine[128] = {
   0x4c,0x0d,0x08
 };
 
-char dma_load_routine[128 + 1024] = {
+char dma_load_routine[256 + 1024] = {
 
   // Routine that copies packet contents by DMA
   0xa9,0x00, // Dummy LDA #$xx for signature detection
   0x8d, 0x07, 0xd7, // STA $D707 to trigger in-line DMA
-  
-  
+    
 #define DMALIST_OFFSET (2+3)
   // SRC MB is $FF
   0x80,0xff,
@@ -78,8 +77,8 @@ char dma_load_routine[128 + 1024] = {
   
   0x04, 
 #define BYTE_COUNT_OFFSET (DMALIST_OFFSET+6)
-  0x00, 0x04,       // DMA byte count ($0031-$0032)
-  0x80, 0xe8, 0x8d, // DMA source address (points to data in packet)
+  0x00, 0x04,       // DMA byte count
+  0x00, 0xe9, 0x8d, // DMA source address (points to data in packet)
 #define DESTINATION_ADDRESS_OFFSET (DMALIST_OFFSET+11)
   0x00, 0x10, // DMA Destination address (bottom 16 bits)
 #define DESTINATION_BANK_OFFSET (DMALIST_OFFSET+13)
@@ -90,48 +89,58 @@ char dma_load_routine[128 + 1024] = {
   // Use chained DMA to copy packet to TX buffer, and then send it
   // so that we can get what will effectively be an ACK to each
   // packet received by the MEGA65.
-  // 1. Copy source eth to dest eth
-  // 2. Copy dest eth to source eth
-  // 3. Copy packet body
-  // 4. Set packet len
-  // 5. Trigger ethernet TX
-
-  // 1. Copy source eth to dest eth  
-  0x80, 0xff,        // SRC MB is $FF  
-  0x81, 0xff,        // Destination MB is $FF
-  0x00,              // DMA end of option list
-  0x04,              // Chained DMA copy
-  0x06, 0x00,        // DMA byte count 
-  0x08, 0xe8, 0x8d,  // DMA source address
-  0x00, 0xe0, 0x8d,  // DMA destination address
-  0x00,              // DMA Sub command
-  0x00, 0x00,        // DMA modulo (ignored)
-
-  // 2. Copy dest eth to source eth
-  0x00,              // DMA end of option list
-  0x04,              // Chained DMA copy
-  0x06, 0x00,        // DMA byte count 
-  0x02, 0xe8, 0x8d,  // DMA source address
-  0x06, 0xe0, 0x8d,  // DMA destination address
-  0x00,              // DMA Sub command
-  0x00, 0x00,        // DMA modulo (ignored)
-
-  // 3. Copy packet body
+  0x80,0xff,
+  0x81,0xff,
   0x00,              // DMA end of option list
   0x00,              // DMA copy, end of chain
-  0x00, 0x05,        // DMA byte count 
-  0x0e, 0xe8, 0x8d,  // DMA source address
-  0x0c, 0xe0, 0x8d,  // DMA destination address
+  0x00, 0x06,        // DMA byte count 
+  0x02, 0xe8, 0x8d,  // DMA source address
+  0x00, 0xe8, 0x8d,  // DMA destination address
   0x00,              // DMA Sub command
   0x00, 0x00,        // DMA modulo (ignored)
   
   // Code resumes after DMA list here
-  // 4. Set packet len
-  //  0xa9,0x00, (A already loaded to zero earlier)
+
+  // Reverse port numbers
+  0xad,0x24,0x68,
+  0x8d,0x24,0x68,
+  0xad,0x25,0x68,
+  0x8d,0x25,0x68,
+
+  0xad,0x26,0x68,
+  0x8d,0x22,0x68,
+  0xad,0x27,0x68,
+  0x8d,0x23,0x68,
+
+  // Copy source MAC to dest MAC
+  0xad,0x08,0x68,0x8d,0x00,0x68,
+  0xad,0x09,0x68,0x8d,0x01,0x68,
+  0xad,0x0a,0x68,0x8d,0x02,0x68,
+  0xad,0x0b,0x68,0x8d,0x03,0x68,
+  0xad,0x0c,0x68,0x8d,0x04,0x68,
+  0xad,0x0d,0x68,0x8d,0x05,0x68,
+  
+  // Set our MAC address
+  0xad,0xe9,0xd6,0x8d,0x06,0x68,
+  0xad,0xea,0xd6,0x8d,0x07,0x68,
+  0xad,0xeb,0xd6,0x8d,0x08,0x68,
+  0xad,0xec,0xd6,0x8d,0x09,0x68,
+  0xad,0xed,0xd6,0x8d,0x0a,0x68,
+  0xad,0xee,0xd6,0x8d,0x0b,0x68,
+  
+  // Set packet len
+  0xa9,0x2a, 
   0x8d,0xe2,0xd6,
   0xa9,0x05,
   0x8d,0xe3,0xd6,
 
+  // Set source IP last byte to 65
+  0xa9,0x41,
+  0x8d,0x1d,0x68,
+  // Set dest IP last byte to that of sender
+  0xad,0x1f,0x68,
+  0x8d,0x21,0x68,
+  
   // 5. TX packet
   0xa9,0x01,
   0x8d,0xe4,0xd6,
@@ -139,10 +148,7 @@ char dma_load_routine[128 + 1024] = {
   // Return to packet wait loop
   0x60, // RTS 
   
-// Packet ID number at offset $003B
-#define PACKET_NUMBER_OFFSET 0x7e
-  0x30,0x00,
-#define DATA_OFFSET (0x80 - 0x2c)
+#define DATA_OFFSET (0x100 - 0x2c)
 };
 
 unsigned char colour_ram[1000];
@@ -231,7 +237,6 @@ int send_mem(unsigned int address,unsigned char *buffer,int bytes)
   // by allowing some window of un-acked frames. But this will do for now.
   usleep(1500);
   
-  dma_load_routine[PACKET_NUMBER_OFFSET]++;
   return 0;
 }
 
