a15609c4a8c0be27abedfa0aee395a6e9ee1c54b
Robert Steffens <39485357+ki-bo@users.noreply.github.com>
2023-01-17 10:26:18 +0100

    Update test 646 to use freeze slot 0 sectors for testing

--- src/tests/test_646.c
+++ src/tests/test_646.c
@@ -1,12 +1,16 @@
 /*
-  Multi-sector writes were not writing the last sector (command $06), but 
+  Multi-sector writes were not writing the last sector (command $06), but
   instead the bytes of the sector buffer were accidently interpreted as
   SD controller commands, which could lead to random behaviour. Usually,
   the SD card gets stuck in busy status at some point.
 
   This routine tests writing multiple batches sequential sectors using
   multi-sector write commands. It saves the overwritten sectors first
-  and restores them with single sector write commands at the end. 
+  and restores them with single sector write commands at the end.
+
+  Without the fix for this issue in place, there is a chance the SD
+  card gets stuck in busy status. This is detected via a timeout
+  and reported as failed test, as well.
 */
 #define ISSUE_NUM 646
 #define ISSUE_NAME "multi-sector sd card write omits last sector"
@@ -20,7 +24,7 @@
 char msg[80 + 1];
 
 int8_t test_status = -1;
-static const uint32_t start_sector = 171464UL;
+uint32_t start_sector = 0;
 static const uint8_t num_batches = 10;
 static const uint8_t num_sectors_per_batch = 5;
 static uint8_t sector_buffer[512];
@@ -34,14 +38,41 @@ void init(void)
   POKE(0xD020, 0);
   POKE(0xD021, 0);
 
-  POKE(0xD689, PEEK(0xD689)|128); // Enable SD buffer instead of floppy buffer
+  POKE(0xD689, PEEK(0xD689) | 128); // Enable SD buffer instead of floppy buffer
 }
 
 void wait_for_sd_ready()
 {
+  uint8_t timeout_frames = 100;
+  uint8_t current_frame, last_frame = PEEK(0xD7FA);
   // Both busy flags in bits 0 and 1 need to be cleared
-  while (PEEK(0xD680) & 0x03)
-    POKE(0xD020, PEEK(0xD020)+1);
+  while (PEEK(0xD680) & 0x03) {
+    POKE(0xD020, PEEK(0xD020) + 1);
+    current_frame = PEEK(0xD7FA);
+    if (current_frame != last_frame) {
+      last_frame = current_frame;
+      --timeout_frames;
+      if (timeout_frames == 0) {
+        test_status = 3;
+        return;
+      }
+    }
+  }
+  return;
+}
+
+void get_slot_zero_start_sector()
+{
+  *(uint32_t *)0xD681 = 0;
+#pragma optimize(push, off)
+  __asm__("ldx #$00");
+  __asm__("ldy #$00");
+  __asm__("lda #$10");
+  __asm__("sta $d642");
+  __asm__("clv");
+#pragma optimize(pop)
+
+  start_sector = *(uint32_t *)0xD681;
 }
 
 void backup_sectors()
@@ -51,16 +82,22 @@ void backup_sectors()
   uint32_t backup_address = 0x40000UL;
 
   wait_for_sd_ready();
+  if (test_status == 3)
+    return;
   while (sectors_left > 0) {
     *(uint32_t *)0xD681 = cur_sector;
     POKE(0xD680, 0x02);
     wait_for_sd_ready();
+    if (test_status == 3)
+      return;
     lcopy(0xffd6e00, backup_address, 512);
     backup_address += 512;
     ++cur_sector;
     --sectors_left;
   }
   wait_for_sd_ready();
+  if (test_status == 3)
+    return;
 }
 
 void restore_sectors()
@@ -71,6 +108,8 @@ void restore_sectors()
 
   while (sectors_left > 0) {
     wait_for_sd_ready();
+    if (test_status == 3)
+      return;
     lcopy(backup_address, 0xffd6e00, 512);
     *(uint32_t *)0xD681 = cur_sector;
     POKE(0xD680, 0x57);
@@ -80,6 +119,8 @@ void restore_sectors()
     --sectors_left;
   }
   wait_for_sd_ready();
+  if (test_status == 3)
+    return;
 }
 
 void multi_sector_write()
@@ -91,6 +132,8 @@ void multi_sector_write()
   uint32_t batch_start_sector = start_sector;
 
   wait_for_sd_ready();
+  if (test_status == 3)
+    return;
   while (batches_left > 0) {
     *(uint32_t *)0xD681 = batch_start_sector;
     while (sectors_left > 0) {
@@ -104,6 +147,8 @@ void multi_sector_write()
       POKE(0xD680, 0x57);
       POKE(0xD680, cmd);
       wait_for_sd_ready();
+      if (test_status == 3)
+        return;
       ++data_byte;
       --sectors_left;
     }
@@ -123,10 +168,14 @@ void verify_sectors_written()
   uint8_t *ptr;
 
   wait_for_sd_ready();
+  if (test_status == 3)
+    return;
   while (sectors_left > 0) {
     *(uint32_t *)0xD681 = cur_sector;
     POKE(0xD680, 0x02);
     wait_for_sd_ready();
+    if (test_status == 3)
+      return;
     lcopy(0xffd6e00, (uint32_t)sector_buffer, 512);
     ptr = sector_buffer;
     for (i = 0; i < 512; ++i) {
@@ -141,6 +190,8 @@ void verify_sectors_written()
     --sectors_left;
   }
   wait_for_sd_ready();
+  if (test_status == 3)
+    return;
   test_status = 0;
 }
 
@@ -151,15 +202,28 @@ void main(void)
 
   unit_test_setup(ISSUE_NAME, ISSUE_NUM);
 
-  printf("Backing up sectors\n");
-  backup_sectors();
-  printf("Writing sectors\n");
-  multi_sector_write();
-  printf("Verifing data\n");
-  verify_sectors_written();
-  printf("Restoring sectors\n");
-  restore_sectors();
-  printf("Done\n");
+  printf("Get freeze slot 0 sector from Hyppo\n");
+  get_slot_zero_start_sector();
+  if (start_sector == 0) {
+    test_status = 2;
+  }
+  else {
+    printf("Backing up sectors\n");
+    backup_sectors();
+    if (test_status != 3) {
+      printf("Writing sectors\n");
+      multi_sector_write();
+    }
+    if (test_status != 3) {
+      printf("Verifing data\n");
+      verify_sectors_written();
+    }
+    if (test_status != 3) {
+      printf("Restoring sectors\n");
+      restore_sectors();
+      printf("Done\n");
+    }
+  }
 
   switch (test_status) {
   case 0:
@@ -168,6 +232,14 @@ void main(void)
     break;
   case 1:
     snprintf(msg, 80, "error in multi-sector write operation");
+    unit_test_fail(msg);
+    break;
+  case 2:
+    snprintf(msg, 80, "error getting freeze slot #0 sector");
+    unit_test_fail(msg);
+    break;
+  case 3:
+    snprintf(msg, 80, "sd card stuck in busy state");
     unit_test_fail(msg);
     break;
   default:
