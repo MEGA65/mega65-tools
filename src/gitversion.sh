dccb2bce3e3d32ad70970a993e4f7dc2a8ac9296
Oliver Graf <oliver@m-e-g-a.org>
2024-07-05 13:04:12 +0200

    feat(src/gitversion.sh): change to mega65-core style versioning

--- src/gitversion.sh
+++ src/gitversion.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
-# status of branch in short format
-# work around travis and jenkins doing things differently
+shorten_name () {
+  name=$1
+  # issue branch?
+  if [[ $name =~ ^([0-9]+)-(.+)$ ]]; then
+    name="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
+  fi
+  # developemnt + extra?
+  if [[ $name =~ ^development-(.+)$ ]]; then
+    name="dev-${BASH_REMATCH[1]}"
+  fi
+  # release + version?
+  if [[ $name =~ ^release-([0-9])\.([0-9][0-9]?)$ ]]; then
+    name="r-${BASH_REMATCH[1]}.${BASH_REMATCH[2]}000"
+  fi
+  # cut after 6 chars
+  echo ${name:0:6}
+}
+
+
 if [[ -n $JENKINS_SERVER_COOKIE ]]; then
-    branch2=${BRANCH_NAME:0:6}
-    version=${GIT_COMMIT:0:7}
-    buildnum=${BUILD_NUMBER}
+  branch=${BRANCH_NAME}
+  version=${GIT_COMMIT:0:7}
+  buildnum=${BUILD_NUMBER}
 else
-    if [[ -n $TRAVIS_BRANCH ]]; then
-        branch=$TRAVIS_BRANCH
-        buildnum=$TRAVIS_BUILD_NUMBER
-    else
-        branch=$(git rev-parse --abbrev-ref HEAD)
-        buildnum=man
-    fi
-    # we only take the first 6 chars
-    branch2=${branch:0:6}
-    # get commit (7 chars plus optional ~ for dirty)
-    version=$(git describe --always --abbrev=7 --dirty=~)
+  if [[ -n $TRAVIS_BRANCH ]]; then
+    branch=$TRAVIS_BRANCH
+    buildnum=$TRAVIS_BUILD_NUMBER
+  else
+    branch=`git rev-parse --abbrev-ref HEAD`
+    buildnum=man
+  fi
+  # get commit (7 chars plus optional ~ for dirty)
+  version=$(git describe --always --abbrev=7 --dirty=~)
 fi
+branch2=$(shorten_name $branch)
+
 # get date plus hour
 datetime=$(date +%Y%m%d.%H)
 
