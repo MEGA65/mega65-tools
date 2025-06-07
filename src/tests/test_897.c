5a1f39b8dd3a40b7aebb900c73861d79e8d9a3a6
Paul Gardner-Stephen <paul@m-e-g-a.org>
2025-06-08 02:05:07 +0930

    expand 897 tests

--- src/tests/test_897.c
+++ src/tests/test_897.c
@@ -8,14 +8,18 @@ Issue #897 -
 #include <stdint.h>
 #include <memory.h>
 #include <tests.h>
+#include <hal.h>
 
 extern int test_resource_read(void);
 
 char msg[80];
 int i = 0;
+unsigned char carry = 0;
 
 unsigned long resource_sector = 0;
 unsigned long return_value = 0;
+
+char message[81];
 
 void main(void)
 {
@@ -27,7 +31,8 @@ void main(void)
 
   unit_test_setup(ISSUE_NAME, ISSUE_NUM);
 
-  //
+  // Try reading sector 0 of resource area
+  resource_sector = 0;
   *(unsigned long *)0x7f0 = resource_sector;
   
   if (test_resource_read() == 0) {
@@ -39,15 +44,96 @@ void main(void)
 
   return_value = *(unsigned long *)0x7f4;
 
-  printf("Return value = $%08lx, P=$%02x, Carry=%d\n",
-	 return_value,PEEK(0x7f8),
-	 PEEK(0x7f8)&0x01);
-  
-  if (i == 256) {
-    unit_test_ok("hyppo resource read test passed");
-  } else {
-    unit_test_ok("hyppo resource read test passed");
+  carry = PEEK(0x7f8)&1;
+
+  if (!carry) {
+    switch(PEEK(0x7f4)) {
+    case 0xff: unit_test_fail("hyppo returned 'trap not implemented'"); break;
+    case 0x11: // dos_errorcode_illegal_value
+      unit_test_fail("reading resource sector 0 failed -- no resource area in system partition?");
+      break;
+    default: {
+      snprintf(message,80,"hyppo resource read failed with unknown error $%02x",PEEK(0x7f4));
+      unit_test_fail(message);
+    }
+    }
+    unit_test_report(ISSUE_NUM, 0, TEST_DONEALL);
+    return;
   }
 
+  unit_test_ok("hyppo resource read sector 0 test passed");
+
+  resource_sector = 0xffffffffUL;
+  for(i=31;i>=0;i--) {
+    *(unsigned long *)0x7f0 = resource_sector;
+
+    if (test_resource_read() != 0) unit_test_fail("hyppo trap failed");
+    carry = PEEK(0x7f8)&1;
+
+    if (!carry) {
+      resource_sector -= (1UL <<i);
+    }
+
+  }
+
+  if (resource_sector==0xffffffffUL || (!resource_sector)) {
+    unit_test_fail("failed to determine size of shared resource area");
+  } else {
+    snprintf(message,80,"determined shared resource area is $%08lx sectors",
+	     resource_sector + 1);
+    unit_test_ok(message);
+  }
+  
+  printf("Shared resource area = $%08lx sectors.\n",resource_sector+1);
+
+
+  // Reset SD card and make sure it's not busy after, so that we can write to the sector buffer
+  // so that we can be sure that the sector buffer contents change after a read request.
+  POKE(0xD680,0x00);
+  POKE(0xD680,0x01);
+  // Wait for SD card busy to clear
+  for(i=0;i<32767;i++) {
+    if ((PEEK(0xD680)&3)==0x00) break;
+    usleep(100);
+  }
+  if (i==32767) unit_test_fail("sd card stayed busy on reset");
+  else unit_test_ok("sd card busy cleared on reset");
+
+  
+  // Now check that requests cause $D681-4 get overwritten
+  // And the SD sector buffer changes contents
+  POKE(0xD681,0x00);
+  POKE(0xD682,0x00);
+  POKE(0xD683,0x00);
+  POKE(0xD684,0x00);
+  lpoke(0xffd6000UL,0x42);
+  lpoke(0xffd6001UL,0x23);
+  
+  resource_sector = 0;
+  *(unsigned long *)0x7f0 = resource_sector;
+  
+  if (test_resource_read() != 0) unit_test_fail("hyppo trap failed");
+  carry=0;
+  if (PEEK(0xD681)!=0x00) carry=1;
+  if (PEEK(0xD682)!=0x00) carry=1;
+  if (PEEK(0xD683)!=0x00) carry=1;
+  if (PEEK(0xD684)!=0x00) carry=1;
+  if (!carry) unit_test_fail("sd card registers not written to");
+  else unit_test_ok("sd card registers written to");
+
+  // Wait for SD card busy to clear
+  for(i=0;i<32767;i++) {
+    if ((PEEK(0xD680)&3)==0x00) break;
+    usleep(100);
+  }
+  if (i==32767) unit_test_fail("sd card stayed busy");
+  else unit_test_ok("sd card busy cleared");
+  
+  carry=0;
+  if (lpeek(0xffd6000UL)!=0x42) carry=1;
+  if (lpeek(0xffd6001UL)!=0x23) carry=1;
+  if (!carry) unit_test_fail("sd card buffer contents did not change");
+  else unit_test_ok("sd card buffer contents change");
+  
   unit_test_report(ISSUE_NUM, 0, TEST_DONEALL);
 }
